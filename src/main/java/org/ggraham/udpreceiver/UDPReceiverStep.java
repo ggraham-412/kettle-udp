9c33100a0ad41d10194907f1aed35a84df8471e7
Greg Graham <ggraham412@yahoo.com>
2018-01-03 14:44:57 -0600

    Resolves some import issues in the latest snapshot.

--- src/main/java/org/ggraham/udpreceiver/UDPReceiverStep.java
+++ src/main/java/org/ggraham/udpreceiver/UDPReceiverStep.java
@@ -33,7 +33,6 @@ import org.pentaho.di.trans.step.StepDataInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
-import org.pentaho.di.ui.spoon.SharedObjectSyncUtil.SynchronizationHandler;
 
 import java.io.ByteArrayInputStream;
 import java.io.ObjectInputStream;
